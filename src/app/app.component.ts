ef6f5eac5bb421876b72d3f8848c9894ac76d217
Gabriel G <base@gbr.pw>
2020-11-15 12:48:50 -0500

    Listado de items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pasteleria11';
+
+  items = [
+    {
+      title: 'Muffin de agraz',
+      price: 5000,
+      category: 'muffins'
+    },
+    {
+      title: 'Muffin de chocolate',
+      price: 5100,
+      category: 'muffins'
+    },
+    {
+      title: 'Muffin de vainilla',
+      price: 5200,
+      category: 'muffins'
+    },
+    {
+      title: 'Cupcake con toppings',
+      price: 5300,
+      category: 'cupcakes'
+    },
+    {
+      title: 'Cupcake de chocolate',
+      price: 5400,
+      category: 'cupcakes'
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SearchComponent } from './components/search/search.component';
+import { ItemComponent } from './components/item/item.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
